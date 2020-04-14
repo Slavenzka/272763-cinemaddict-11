5a2184df1fc03a043d5fb8021a7832cc98787597
Ivan <i.romanov@protonmail.com>
2020-04-14 10:09:19 +0300

    Removed blank line

--- src/components/site-navigation.js
+++ src/components/site-navigation.js
@@ -2,7 +2,6 @@ const createFilterMarkup = (name, isActive, count) => {
   const type = name.split(` `)[0].toLowerCase();
   const isItemActive = isActive ? `main-navigation__item--active` : ``;
   const itemCount = count && type !== `all` ? `<span class="main-navigation__item-count">${count}</span>` : ``;
-
   return (
     `
       <a href="#${type}" class="main-navigation__item ${isItemActive}">${name} ${itemCount}</a>
