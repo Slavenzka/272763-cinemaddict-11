213dd1ee802c1757e31458a6d331a2be28fbf754
Ivan <i.romanov@protonmail.com>
2020-05-04 11:13:51 +0300

    Add film model

--- src/mock/cards.js
+++ src/mock/cards.js
@@ -47,6 +47,7 @@ const generateFilm = () => {
   }
 
   // mock self explaining properties
+  filmItem.id = `Film item id#${Math.floor(Math.random() * 1000000000)} / ${new Date()}`;
   filmItem.nameOriginal = `${filmItem.name}`;
   filmItem.descriptionPreview = filmDescription;
   filmItem.rating = getRandomNumberInRange(0, maxRating, 1);
